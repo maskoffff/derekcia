549f8141ede637719bfa9b5e0201afd1535a16a0
noruwa03 <noruwaobaseki@gmail.com>
2020-12-20 11:51:55 +0100

    checkout works

---
+++ javascript/func.js
@@ -0,0 +1,7 @@
+const menu = document.querySelector(".menu");
+const sidebar = document.querySelector(".sidebar");
+
+menu.onclick = function() {
+    menu.classList.toggle("active");
+    sidebar.classList.toggle("active");
+}
