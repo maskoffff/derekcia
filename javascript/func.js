d8af8d3dc79fdbdc3f16c8e71e606a2e294b9b1d
noruwa03 <noruwaobaseki@gmail.com>
2021-01-12 11:07:53 +0100

    Responsive Watch Design

--- javascript/func.js
+++
@@ -1,7 +0,0 @@
-const menu = document.querySelector(".menu");
-const sidebar = document.querySelector(".sidebar");
-
-menu.onclick = function() {
-    menu.classList.toggle("active");
-    sidebar.classList.toggle("active");
-}
